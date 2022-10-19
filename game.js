086cc2696cd0301ba58ea2e80c85a8747041f329
maxikaye <mr.max.kaye@gmail.com>
2022-10-18 21:06:59 -0400

    Add results to html dynamically

--- game.js
+++ game.js
@@ -4,6 +4,14 @@
 
 let playerScore = 0;
 let computerScore = 0;
+let roundResult = null;
+
+const rock = document.querySelector('#btn-rock');
+const paper = document.querySelector('#btn-paper');
+const scissors = document.querySelector('#btn-scissors');
+
+const results = document.querySelector('.results');
+const resultsText = document.createElement('p');
 
 function getComputerChoice() {
     const weapons = ["rock", "paper", "scissors"];
@@ -16,49 +24,55 @@ function getPlayerChoice() {
    return playerChoice.toLowerCase(); 
 }
 
+function showResults() {
+    resultsText.textContent = roundResult;
+    results.appendChild(resultsText);
+}
+
 function playRound(playerSelection, computerSelection) {
-    let result = null;
+
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            result = "Tie! Rock bounces off of Rock";
+            roundResult = "Tie! Rock bounces off of Rock";
         } else if (computerSelection === "paper") {
-            result = "You lose! Paper covers Rock"
+            roundResult = "You lose! Paper covers Rock"
             computerScore += 1;
         } else if (computerSelection === "scissors") {
-            result = "You win! Rock smashes scissors";
+            roundResult = "You win! Rock smashes scissors";
             playerScore += 1;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            result = "You win! Paper covers Rock";
+            roundResult = "You win! Paper covers Rock";
             playerScore += 1;
         } else if (computerSelection === "paper") {
-            result = "Tie! Paper bounces off Paper"
+            roundResult = "Tie! Paper bounces off Paper"
         } else if (computerSelection === "scissors") {
-            result = "You lose! Scissors cuts Paper";
+            roundResult = "You lose! Scissors cuts Paper";
             computerScore += 1;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            result = "You lose! Rock smashes scissors";
+            roundResult = "You lose! Rock smashes scissors";
             computerScore += 1;
         } else if (computerSelection === "paper") {
-            result = "You win! Scissors cuts Paper"
+            roundResult = "You win! Scissors cuts Paper"
             playerScore += 1;
         } else if (computerSelection === "scissors") {
-            result = "Tie! Scissors bounces off of Scissors";
+            roundResult = "Tie! Scissors bounces off of Scissors";
         }
     } else {
-        result = `${playerSelection} is not a valid weapon.`
+        roundResult = `${playerSelection} is not a valid weapon.`
     }
-    console.log(result);
+
+    showResults();
 }
 
 /* function game() {
     const rounds = 5;
     for (let i = 0; i < rounds; i++) {
-        let roundResult = playRound(getPlayerChoice(), getComputerChoice());
-        console.log(`${roundResult} | Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+        let roundroundResult = playRound(getPlayerChoice(), getComputerChoice());
+        console.log(`${roundroundResult} | Player Score: ${playerScore} | Computer Score: ${computerScore}`);
     }
     if (playerScore > computerScore) {
         console.log(`You Win! Final Score: ${playerScore} | ${computerScore}`);
@@ -77,10 +91,6 @@ function reset() {
 }
 
 reset();
-
-const rock = document.querySelector('#btn-rock');
-const paper = document.querySelector('#btn-paper');
-const scissors = document.querySelector('#btn-scissors');
 
 rock.addEventListener('click', e => playRound(e.target.name, getComputerChoice()));
 paper.addEventListener('click', e => playRound(e.target.name, getComputerChoice()));
